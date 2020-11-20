ec718dd01dec4ee7e7037fccd822ba91d9ce628d
felipetodev <fe.ossandon.u@gmail.com>
2020-11-19 23:34:40 -0300

    start

---
+++ src/api.js
@@ -0,0 +1,36 @@
+const base_url = 'https://api.rawg.io/api/'
+
+//Fecha
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1
+    if(month < 10) {
+        return `0${month}`
+    } else {
+        return month
+    }
+}
+
+const getCurrentDay = () => {
+    const day = new Date().getDate() + 1
+    if(day < 10) {
+        return `0${day}`
+    } else {
+        return day
+    }
+}
+
+const currentYear = new Date().getFullYear()
+const currentMonth = getCurrentMonth()
+const currentDay = getCurrentDay()
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
+
+
+//Juegos populares
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+
+const popularGamesURL = () => `${base_url}${popular_games}`
+
+// console.log(popularGamesURL())
